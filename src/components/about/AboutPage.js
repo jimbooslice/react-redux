2bf8d724da417ced65f1bd4e15853f394aa1ebeb
Jimmy Li <lij5@mskcc.org>
2018-12-27 23:03:05 -0500

    fin ch.5

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class AboutPage extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>About</h1>
+                <p>THis application uses React, Redux, React Router and a variety of other things</p>
+            </div>
+        )
+    }
+}
